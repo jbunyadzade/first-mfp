97386f53b7ad2818d20785dee4702eb1a0cd51d0
Javid Bunyadzade <javid.bunyadzade@pashabank.az>
2021-03-13 14:37:22 +0400

    init commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { MarketingApp } from './components/MarketingApp';
+
+export const App = () => {
+    return (
+        <div>
+            <h1>Hello there</h1>
+            <MarketingApp />
+        </div>
+    );
+};
